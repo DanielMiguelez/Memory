2375a694021edd8714d21b9b3182d83276737918
Usuario <Usuario@A9PC09>
2024-03-22 19:10:47 +0100

    Controller in progress.

--- src/funcionamientoJuego/testJuego.java
+++ src/funcionamientoJuego/testJuego.java
@@ -8,6 +8,29 @@ package funcionamientoJuego;
 import java.util.Scanner;
 
 public class testJuego {
+    
+     // Función para mostrar el tablero
+    
+    public static void mostrarTablero(Card[][] matrizCartas, boolean[][] cartasDescubiertas) {
+        int filas = matrizCartas.length;
+        int columnas = matrizCartas[0].length;
+        
+        // Bucle para iterar sobre cada fila y columna del tablero
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j++) {
+                if (cartasDescubiertas[i][j]) {
+                    System.out.print(matrizCartas[i][j].getName() + " ");
+                } else {
+                    System.out.print("Boca Abajo ");
+                }
+            }
+            System.out.println(); // Nueva línea para la próxima fila
+        }
+    }
+    
+    //-----------------------------------------------------------------------------------------------
+    
+    
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
@@ -52,14 +75,23 @@ public class testJuego {
         
         // Barajar el mazo
         deck.shuffle();
-
-        // Iteramos para obtener las cartas y su posición, volteadas o no volteadas.
-        for (int i = 0; i < deck.getCards().size(); i++) {
-            Card card = deck.getCards().get(i);
-            System.out.println("Nombre de la carta: " + card.getName());
-            System.out.println("Estado de la carta: " + card.isUncovered());
-            System.out.println(); // Espacio en blanco para mejorar la legibilidad
+        
+        // Declarar la matriz para representar las cartas
+        
+        int filas = 4; // Determinamos el numero de filas.
+        int columnas = 4; // Determinamos las columnas.
+        
+        Card[][] matrizCartas = new Card[filas][columnas];
+        boolean[][] cartasDescubiertas = new boolean[filas][columnas];
+        
+        int cartaIndex = 0;
+        for(int i = 0; i<filas; i++){
+            for(int j= 0; j< columnas; j++){
+                matrizCartas[i][j] = deck.getCards().get(cartaIndex);
+                cartaIndex++;
+            }
         }
+        
         
         // INICIAMOS EL JUEGO ----------------
         
@@ -67,11 +99,98 @@ public class testJuego {
         
         int puntosJugador1 = 0;
         int puntosJugador2 = 0;
+        int turnoJugador = 1;
+        int intentosJugador1 = 0;
+        int intentosJugador2 = 0;
+        int parejasAcertadas = 0;
+        int parejasTotales = filas*columnas /2;
         
-        //while(!juegoTerminado){
+        while (!juegoTerminado) {
             
-        //}
+        // Mostrar el tablero actual
+   
+        System.out.println("Tablero actual:");
         
+        mostrarTablero(matrizCartas, cartasDescubiertas);
+         
+            //Aqui empieza el turno del jugador numero 1
+            
+            System.out.println("Turno del jugador " + jugador1.getName());
+            
+            System.out.println("Selecciona la fila de la primera carta:");
+            int filaCarta1 = sc.nextInt();
+            
+            System.out.println("Introduce la columna de la primera carta");
+            int columnaCarta1 = sc.nextInt();
+            
+            cartasDescubiertas[filaCarta1][columnaCarta1] = true;
+            
+            
+            System.out.println("Selecciona la fila de la segunda carta:");
+            int filaCarta2 = sc.nextInt();
+            
+            System.out.println("Introduce la columna de la segunda carta");
+            int columnaCarta2 = sc.nextInt();
+            
+            cartasDescubiertas[filaCarta2][columnaCarta2] = true;
+            
+            // Tablero despues del turno del primer jugador
+            
+            System.out.println("Tablero después del turno de " + jugador1.getName() + ":");
+            
+            mostrarTablero(matrizCartas, cartasDescubiertas);
+           
+            
+            //Aqui empieza el turno del jugador numero 2
+            
+            System.out.println("Turno del jugador " + jugador2.getName());
+            
+            System.out.println("Selecciona la fila de la primera carta:");
+            int filaCarta3 = sc.nextInt();
+            
+            System.out.println("Introduce la columna de la primera carta");
+            int columnaCarta3 = sc.nextInt();
+            
+            cartasDescubiertas[filaCarta3][columnaCarta3] = true;
+            
+            
+            System.out.println("Selecciona la fila de la segunda carta:");
+            int filaCarta4 = sc.nextInt();
+            
+            System.out.println("Introduce la columna de la segunda carta");
+            int columnaCarta4 = sc.nextInt();
+            
+            cartasDescubiertas[filaCarta4][columnaCarta4] = true;
+            
+            // Tablero despues del turno del primer jugador
+            
+            System.out.println("Tablero después del turno de " + jugador2.getName() + ":");
+             
+            mostrarTablero(matrizCartas, cartasDescubiertas);
+              
+            
+            if (matrizCartas[filaCarta1][columnaCarta1].getName().equals(matrizCartas[filaCarta3][columnaCarta3].getName())
+                && matrizCartas[filaCarta2][columnaCarta2].getName().equals(matrizCartas[filaCarta4][columnaCarta4].getName())) {
+                
+                 // Las cartas son iguales
+            System.out.println("¡Las cartas son iguales!");
+            // Incrementar el puntaje del jugador actual
+            
+                if (turnoJugador == 1) {
+                    puntosJugador1++;
+                } else {
+                    puntosJugador2++;
+                }
+                
+            } else {
+                // Las cartas no son iguales, volver a ponerlas boca abajo
+                cartasDescubiertas[filaCarta1][columnaCarta1] = false;
+                cartasDescubiertas[filaCarta2][columnaCarta2] = false;
+                cartasDescubiertas[filaCarta3][columnaCarta3] = false;
+                cartasDescubiertas[filaCarta4][columnaCarta4] = false;
+                System.out.println("Las cartas no son iguales. ¡Inténtalo de nuevo!");
+            }  
+        }  
     }
 }
 
