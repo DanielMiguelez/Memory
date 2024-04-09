be89818860565ef60a43a16078c0a38bf9c3901f
nocxil <160049420+nocxil@users.noreply.github.com>
2024-04-09 19:34:04 +0200

    Revert "Controller in process, only isTurned and register inside the loop left."
    This reverts commit 1e42ffddfa185a80f049dc038b03bc1c0b3e063b.

--- src/memory/Card.java
+++ src/memory/Card.java
@@ -1,60 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package memory;
 
 public class Card {
-
+    
     public String name;
     private int id;
     private boolean turned;
-    private boolean matched;
     private String image;
-
+    
     public Card (String name, int id){
         this.name = name;
         this.id = id;
         this.turned = false;
         this.image = image;
-        this.matched = false;
     }
-
+    
     public String getName(){
         return name;
     }
-
+    
     public int getId(){
         return id;
     }
-
+    
     public boolean isTurned(){
         return turned;
     }
-
-    public boolean isMatched(){
-        return matched;
-    }
-
+    
     public void setTurned(){
         this.turned = true;
     }
-
-    public void setMatched(){
-        this.matched = true;
-    } 
+    
      public boolean getTurned(){
         return turned;
     }
-    public boolean getMatched(){
-        return matched;
-    }
-    public void setCovered(){
+     public void setCovered(){
         this.turned = false;
     }
     public String getImage() {
         return image;
     }
-
+    
     public String toString(){
         String info = "";
-        return info + name + " " + id + " " +turned + " " + matched;
+        return info + name + " " + id + " " +turned;
     }
 
 }
